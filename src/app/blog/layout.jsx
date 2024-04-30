43abedbcb07bce610f0dbcecd0ce064c4960c4e8
samade747 <samad.e747@gmail.com>
2024-04-30 14:28:04 +0500

    8am 30 4 2024

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,13 @@
+const BlogLayout = ({ children }) => {
+    return( 
+    <div>
+     <h2>this is the blog Layout</h2>        
+       {children}     
+        
+        
+        
+    </div>
+    )
+};
+  
+  export default BlogLayout;
