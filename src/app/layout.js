4a1fd55028ca96cff2db53b12208569375463d2f
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-05-01 00:52:26 +0500

    z

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Navbar />
       <h1>this is the main Layout</h1>
       {children}</body>
     </html>
