f32046c8d23fd99585f8f3a25c925b475875fbce
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-05-01 10:27:23 +0500

    1037am 01 05 2024

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import Links from "./links/Links";
+import styles from "../navbar/navbar.module.css"
 
 const Navbar = () => {
 
     return (
-        <div>
+        <div className={styles.container}>
             <div>Logo</div>
             <div>
                 <Links />
