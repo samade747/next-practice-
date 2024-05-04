f8ebcbada51fbc921b61998e9197acaae370f545
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-05-04 07:36:23 +0500

    736am 04 05 2024

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,14 @@
+import Image from "next/image";
+import styles from "about.module.css"
 
 
-
-const aboutpage = () => {
-    return <div>about page</div>;
+const Aboutpage = () => {
+    return <div>
+      <div className={styles.container}>
+      <Image src="/a.png" width={500} height={500} alt="" fill/>
+      </div>
+      
+      </div>;
   };
   
-  export default aboutpage;
+  export default Aboutpage;
