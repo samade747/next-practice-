1a49e191b2f939565595d5a5b2598e802067caab
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-05-04 20:20:19 +0500

    820pm 04 05 2024

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,15 +2,15 @@
 
 
 import Image from "next/image";
-import styles from "about.module.css"
+import styles from "./about.module.css"
 
 
 const Aboutpage = () => {
     return <div>
-      <div className={styles.container}>
-      <Image src="/a.png" width={500} height={500} alt="" fill/>
+      <div className={styles.Container}>
+      <Image src="/a.png"  alt="" />
       </div>
-      
+       
       </div>;
   };
   
