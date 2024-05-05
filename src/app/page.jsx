6ddfe06089ed4563d054d87b9bc46e5c58a2e9b8
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-05-05 18:47:21 +0500

    647pm 05 05 2024

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <h1> Creatve Thoughts Agency</h1>
     
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quibusdam nesciunt nam porro accusantium, repellendus in ea deserunt, non soluta unde beatae illo? Iure magnam autem quae delectus architecto dolorum?
+        <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quibusdam nesciunt nam porro accusantium, repellendus in ea deserunt, non soluta unde beatae illo? Iure magnam autem quae delectus architecto dolorum?
          Id molestias placeat voluptas dolores, reprehenderit ipsam delectus error nulla consequatur, sint modi culpa quibusdam ipsum accusamus beatae iusto est, animi libero. Aspernatur debitis eos ipsa odit suscipit at molestiae.</p>
 
        <div className={styles.buttons}>
