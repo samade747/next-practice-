150d626469c66dcc9e8791eb16c375aae4d73148
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-05-07 00:57:27 +0500

    1257

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,40 +1,39 @@
 import React from "react";
 import styles from "./home.module.css";
-
+import Image from 'next/image';
 
 const Home = () => {
-
   return (
-  <div className={styles.conatiner}>    
-    <div className={styles.textConatiner}>
-        <h1> Creatve Thoughts Agency</h1>
-    
-
-        <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quibusdam nesciunt nam porro accusantium, repellendus in ea deserunt, non soluta unde beatae illo? Iure magnam autem quae delectus architecto dolorum?
-         Id molestias placeat voluptas dolores, reprehenderit ipsam delectus error nulla consequatur, sint modi culpa quibusdam ipsum accusamus beatae iusto est, animi libero. Aspernatur debitis eos ipsa odit suscipit at molestiae.</p>
-
-       <div className={styles.buttons}>
-          <button className={styles.button} >Learn More</button>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quibusdam nesciunt nam porro accusantium, repellendus in ea deserunt, non soluta unde beatae illo? Iure magnam autem quae delectus architecto dolorum? Id molestias placeat voluptas dolores, reprehenderit ipsam delectus error nulla consequatur, sint modi culpa quibusdam ipsum accusamus beatae iusto est, animi libero. Aspernatur debitis eos ipsa odit suscipit at molestiae.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
           <button className={styles.button}>Contact</button>
+        </div>
+
+        <div className={styles.brands}>
+          <Image src="/mern.png" alt="Snow Capped Mountain" fill className={styles.brands}   />          
+       </div>
+
+
+
       </div>
 
-      <div className={styles.brands}>
-          <Image src="/brands.png" alt="Snow Capped Mountain" fill className={styles.branding}   />          
+
+
+
+      <div className={styles.imgContainer}>
+        <Image src="/a.png" width={200} height={200} alt="Snow Capped Mountain" className={styles.heroImg} />
       </div>
-
     </div>
-
-
-
-
-    <div className={styles.imageConatiner}>
-        <Image src="hero.png" alt="Snow Capped Mountain" fill className={styles.heroImg}   />
-    </div>
-    
-    
-    </div>
-    // 11010
-  )
+  );
 };
 
 export default Home;
+
+
+
